4a4f014f4b5bfeea7de2ce212896d052e64c39bf
harrison <d.harrison.lima@gmail.com>
2024-05-28 18:23:02 -0300

    exceptions

--- src/main/java/com/github/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/github/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,7 @@
 package com.github.gestao_vagas.modules.candidate;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -12,12 +13,13 @@ import java.util.UUID;
 public class CandidateEntity {
     private UUID id;
     private String name;
-    private String username;
 
     @Email (message = "O campo [email] deve conter um e-mail válido.")
-
-    @Pattern(regexp = "^(!\\s*$).+", message = "O campo [username] não deve conter espaços")
     private String email;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços")
+    private String username;
 
     @Length(min = 10, max = 100)
     private String password;
